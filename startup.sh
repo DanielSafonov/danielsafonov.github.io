ccff2a3922d664dedc0a32c58d0108eeb5f19220
Daniel Safonov <Safonov.D.Alekse@sberbank.ru>
2019-12-14 18:55:32 +0300

    Update

---
+++ startup.sh
@@ -0,0 +1,3 @@
+echo -e "\n\n\n\033[32mBlog startup\033[0m\n\n\n"
+sudo jekyll build
+sudo jekyll serve
--- upload.sh
+++ upload.sh
@@ -6,4 +6,4 @@ cd _site
 git add *
 git commit -m "Update"
 git push origin master
-echo -e "\n\n\n\033[45mBlog was uploaded successfully\033[0m\n\n\n"
+echo -e "\n\n\n\033[32mBlog was uploaded successfully\033[0m\n\n\n"
